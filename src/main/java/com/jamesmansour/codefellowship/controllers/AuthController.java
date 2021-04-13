dd3b54e9b3b2989a072ec452a55a3620c3c1ca6a
James Mansour <jamestmansour@outlook.com>
2021-04-13 11:26:03 -0700

    Adds css to allowed route for users not signed in.

--- src/main/java/com/jamesmansour/codefellowship/controllers/AuthController.java
+++ src/main/java/com/jamesmansour/codefellowship/controllers/AuthController.java
@@ -3,11 +3,18 @@ package com.jamesmansour.codefellowship.controllers;
 import com.jamesmansour.codefellowship.models.ApplicationUser;
 import com.jamesmansour.codefellowship.repositories.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthController {
@@ -18,18 +25,30 @@ public class AuthController {
     @Autowired
     ApplicationUserRepository applicationUserRepository;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @GetMapping("/signup")
     public String showSingUp(){
         return "signup.html";
     }
 
     @PostMapping("/signup")
-    public RedirectView createUser(String username, String password){
+    public RedirectView createUser(String username, String password, HttpServletRequest request){
         ApplicationUser user = new ApplicationUser();
         user.setPassword(passwordEncoder.encode(password));
         user.setUsername(username);
         applicationUserRepository.save(user);
-        System.out.println("yo");
+
+//      ================ User Created =============
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        token.setDetails(new WebAuthenticationDetails(request));
+        Authentication authentication = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+//      ================ User Signed in =============
+
         return new RedirectView("/");
     }
 
