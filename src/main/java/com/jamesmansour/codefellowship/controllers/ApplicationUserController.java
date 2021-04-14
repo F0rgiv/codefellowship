42915eb5b915dcd33193119fafeae7d34cb130c5
James Mansour <jamestmansour@outlook.com>
2021-04-14 15:01:17 -0700

    Adds ability to view your feed.

--- src/main/java/com/jamesmansour/codefellowship/controllers/ApplicationUserController.java
+++ src/main/java/com/jamesmansour/codefellowship/controllers/ApplicationUserController.java
@@ -1,7 +1,9 @@
 package com.jamesmansour.codefellowship.controllers;
 
 import com.jamesmansour.codefellowship.models.ApplicationUser;
+import com.jamesmansour.codefellowship.models.Post;
 import com.jamesmansour.codefellowship.repositories.ApplicationUserRepository;
+import com.jamesmansour.codefellowship.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +15,13 @@ import org.springframework.web.servlet.view.RedirectView;
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class ApplicationUserController {
 
-    @Autowired
-    ApplicationUserRepository applicationUserRepository;
+    @Autowired ApplicationUserRepository applicationUserRepository;
 
     @GetMapping("/myProfile")
     public RedirectView getUser(HttpServletRequest request){
@@ -31,7 +34,7 @@ public class ApplicationUserController {
         m.addAttribute("user", request.getUserPrincipal().getName());
         ApplicationUser subject = applicationUserRepository.findById(id).get();
         m.addAttribute("subject", subject);
-        return "user-details.html";
+        return "user-details";
     }
 
     @PostMapping("/follow")
@@ -48,5 +51,19 @@ public class ApplicationUserController {
         m.addAttribute("user", request.getUserPrincipal().getName());
         m.addAttribute("users", applicationUserRepository.findAll());
         return "users";
+    }
+
+    @GetMapping("/feed")
+    public String getFeed(Model m, HttpServletRequest request) {
+        ApplicationUser user = applicationUserRepository.findByUsername(request.getUserPrincipal().getName());
+        m.addAttribute("user", user.getUsername());
+        List<Post> posts = new LinkedList<>();
+        for (ApplicationUser person : user.getFollowing()) {
+            for (Post post : person.getPosts()) {
+                posts.add(post);
+            }
+        }
+        m.addAttribute("posts", posts);
+        return "feed";
     }
 }
